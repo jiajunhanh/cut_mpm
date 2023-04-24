69c3e7e1b44cb2ee878543e421ce09d7669e5bd5
Jiajun Han <jiajunhanh@gmail.com>
2023-04-24 17:30:13 +0900

    Handle the case where some grid nodes lie on input edges

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 
 constexpr int n_grid = 3;
+constexpr int n_grid_nodes = (n_grid + 1) * (n_grid + 1);
 constexpr float dx = 1.0f / n_grid;
 
 class Vertex {
@@ -21,6 +22,8 @@ class Vertex {
     [[nodiscard]] float coord(int d) const {
         return static_cast<float>(c[d]) + q[d];
     }
+
+    [[nodiscard]] int node_id() const { return c[1] * (n_grid + 1) + c[0]; }
 };
 
 class Edge {
@@ -29,42 +32,25 @@ class Edge {
     size_t j{}; // point id
 };
 
-Vertex lerp(const Vertex &vi, const Vertex &vj, float t, int d_skip = 2) {
-    Vertex v{};
-    for (int d = 0; d < 2; ++d) {
-        if (d == d_skip) {
-            continue;
-        }
-        auto coord_i = vi.coord(d);
-        auto coord_j = vj.coord(d);
-        auto coord = coord_i + (coord_j - coord_i) * t;
-        v.c[d] = static_cast<int>(std::floor(coord));
-        v.q[d] = coord - std::floor(coord);
-        v.b[d] = v.q[d] == 0.0f;
-    }
-    return v;
+auto lerp(Vertex &v, const Vertex &vi, const Vertex &vj, float t, int d) {
+    auto coord_i = vi.coord(d);
+    auto coord_j = vj.coord(d);
+    auto coord = coord_i + (coord_j - coord_i) * t;
+    v.c[d] = static_cast<int>(std::floor(coord));
+    v.q[d] = coord - std::floor(coord);
+    v.b[d] = v.q[d] == 0.0f;
 }
 
-void add_grid_node_and_edges(std::vector<Vertex> &cut_vertices,
-                             std::vector<Edge> &cut_edges) {
-    auto n = cut_vertices.size();
+void add_grid_edges(std::vector<Vertex> &cut_vertices,
+                    std::vector<Edge> &cut_edges) {
     std::vector<size_t> grid_cut_vertices[n_grid * n_grid][2];
-    for (size_t i = 0; i < cut_vertices.size(); ++i) {
+    for (size_t i = n_grid_nodes; i < cut_vertices.size(); ++i) {
         const auto &v = cut_vertices[i];
         for (int d = 0; d < 2; ++d) {
             if (v.b[d]) {
                 grid_cut_vertices[v.c[1] * n_grid + v.c[0]][d ^ 1].emplace_back(
                     i);
             }
-        }
-    }
-    for (int y = 0; y <= n_grid; ++y) {
-        for (int x = 0; x <= n_grid; ++x) {
-            Vertex v{};
-            v.c[0] = x;
-            v.c[1] = y;
-            v.b[0] = v.b[1] = true;
-            cut_vertices.emplace_back(v);
         }
     }
     for (int y = 0; y < n_grid; ++y) {
@@ -79,25 +65,28 @@ void add_grid_node_and_edges(std::vector<Vertex> &cut_vertices,
                     node_j += n_grid + 1;
                 }
                 if (v.empty()) {
-                    cut_edges.emplace_back(n + node_i, n + node_j);
+                    cut_edges.emplace_back(node_i, node_j);
                     continue;
                 }
                 std::sort(v.begin(), v.end(), [&](size_t i, size_t j) {
                     return cut_vertices[i].q[d] < cut_vertices[j].q[d];
                 });
-                cut_edges.emplace_back(n + node_i, v.front());
+                cut_edges.emplace_back(node_i, v.front());
                 for (size_t i = 0; i < v.size() - 1; ++i) {
                     cut_edges.emplace_back(v[i], v[i + 1]);
                 }
-                cut_edges.emplace_back(v.back(), n + node_j);
+                cut_edges.emplace_back(v.back(), node_j);
             }
         }
     }
     for (int i = 0; i < n_grid; ++i) {
-        cut_edges.emplace_back(n + n_grid + i * (n_grid + 1),
-                               n + n_grid + (i + 1) * (n_grid + 1));
-        cut_edges.emplace_back(n + n_grid * (n_grid + 1) + i,
-                               n + n_grid * (n_grid + 1) + i + 1);
+        cut_edges.emplace_back(n_grid + (n_grid + 1) * i,
+                               n_grid + (n_grid + 1) * (i + 1));
+        cut_edges.emplace_back(n_grid * (n_grid + 1) + i,
+                               n_grid * (n_grid + 1) + i + 1);
+    }
+    for (auto v : cut_vertices) {
+        v.edges.clear();
     }
     for (size_t i = 0; i < cut_edges.size(); ++i) {
         const auto &e = cut_edges[i];
@@ -107,10 +96,20 @@ void add_grid_node_and_edges(std::vector<Vertex> &cut_vertices,
 }
 
 std::pair<std::vector<Vertex>, std::vector<Edge>>
-compute_cut_vertices_and_edges(std::vector<Vertex> &vertices,
-                               std::vector<Edge> &edges) {
-    std::vector<Vertex> cut_vertices(vertices);
+compute_cut_vertices_and_edges(const std::vector<Vertex> &vertices,
+                               const std::vector<Edge> &edges) {
+    std::vector<Vertex> cut_vertices;
     std::vector<Edge> cut_edges;
+    for (int y = 0; y <= n_grid; ++y) {
+        for (int x = 0; x <= n_grid; ++x) {
+            Vertex v{};
+            v.c[0] = x;
+            v.c[1] = y;
+            v.b[0] = v.b[1] = true;
+            cut_vertices.emplace_back(v);
+        }
+    }
+    cut_vertices.insert(cut_vertices.end(), vertices.cbegin(), vertices.cend());
     for (auto &e : edges) {
         std::vector<std::pair<float, size_t>> intersection_points;
         const auto &vi = vertices[e.i];
@@ -130,12 +129,17 @@ compute_cut_vertices_and_edges(std::vector<Vertex> &vertices,
                 auto t = (static_cast<float>(z - vi.c[d]) - vi.q[d]) /
                          (static_cast<float>(vj.c[d] - vi.c[d]) +
                           (vj.q[d] - vi.q[d]));
-                intersection_points.emplace_back(t, cut_vertices.size());
-                auto cut_v = lerp(vi, vj, t, d);
+                Vertex cut_v{};
+                lerp(cut_v, vi, vj, t, d ^ 1);
                 cut_v.c[d] = z;
                 cut_v.q[d] = 0;
                 cut_v.b[d] = true;
+                if (cut_v.b[d ^ 1]) {
+                    intersection_points.emplace_back(t, cut_v.node_id());
+                    continue;
+                }
                 cut_vertices.emplace_back(cut_v);
+                intersection_points.emplace_back(t, cut_vertices.size() - 1);
             }
         }
         std::sort(intersection_points.begin(), intersection_points.end(),
@@ -144,17 +148,23 @@ compute_cut_vertices_and_edges(std::vector<Vertex> &vertices,
                       return a.first < b.first;
                   });
         if (intersection_points.empty()) {
-            cut_edges.emplace_back(e.i, e.j);
+            cut_edges.emplace_back(e.i + n_grid_nodes, e.j + n_grid_nodes);
             continue;
         }
-        cut_edges.emplace_back(e.i, intersection_points.front().second);
+        cut_edges.emplace_back(e.i + n_grid_nodes,
+                               intersection_points.front().second);
         for (size_t i = 0; i + 1 < intersection_points.size(); ++i) {
+            if (intersection_points[i].second ==
+                intersection_points[i + 1].second) {
+                continue;
+            }
             cut_edges.emplace_back(intersection_points[i].second,
                                    intersection_points[i + 1].second);
         }
-        cut_edges.emplace_back(intersection_points.back().second, e.j);
+        cut_edges.emplace_back(intersection_points.back().second,
+                               e.j + n_grid_nodes);
     }
-    add_grid_node_and_edges(cut_vertices, cut_edges);
+    add_grid_edges(cut_vertices, cut_edges);
     return {cut_vertices, cut_edges};
 }
 
