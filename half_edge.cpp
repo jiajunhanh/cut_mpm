e98aad02b2b00565f2cd530db45872d8da6d1044
Jiajun Han <jiajunhanh@gmail.com>
2023-04-24 18:30:54 +0900

    Implement half-edge mesh

--- half_edge.cpp
+++ half_edge.cpp
@@ -1,1 +1,46 @@
 #include "half_edge.h"
+HalfEdgeMesh::HalfEdgeRef HalfEdgeMesh::emplace_half_edge() {
+    half_edges.emplace_back();
+    auto h = std::prev(half_edges.end());
+    h->twin = half_edges.end();
+    h->next = half_edges.end();
+    h->edge = edges.end();
+    h->vertex = vertices.end();
+    h->face = faces.end();
+    h->id = next_id++;
+    return h;
+}
+
+HalfEdgeMesh::VertexRef HalfEdgeMesh::emplace_vertex() {
+    vertices.emplace_back();
+    auto v = std::prev(vertices.end());
+    v->half_edge = half_edges.end();
+    v->id = next_id++;
+    return v;
+}
+
+HalfEdgeMesh::EdgeRef HalfEdgeMesh::emplace_edge() {
+    edges.emplace_back();
+    auto e = std::prev(edges.end());
+    e->half_edge = half_edges.end();
+    e->id = next_id++;
+    return e;
+}
+
+HalfEdgeMesh::FaceRef HalfEdgeMesh::emplace_face() {
+    faces.emplace_back();
+    auto f = std::prev(faces.end());
+    f->half_edge = half_edges.end();
+    f->id = next_id++;
+    return f;
+}
+
+void HalfEdgeMesh::HalfEdgeMesh::free_half_edge(HalfEdgeRef h) {
+    half_edges.erase(h);
+}
+
+void HalfEdgeMesh::HalfEdgeMesh::free_vertex(VertexRef v) { vertices.erase(v); }
+
+void HalfEdgeMesh::HalfEdgeMesh::free_edge(EdgeRef e) { edges.erase(e); }
+
+void HalfEdgeMesh::HalfEdgeMesh::free_face(FaceRef f) { faces.erase(f); }
--- half_edge.h
+++ half_edge.h
@@ -1,31 +1,72 @@
 #pragma once
 
-#include <memory>
+#include <iostream>
+#include <list>
 
-namespace hfeg {
+class HalfEdgeMesh {
+    size_t next_id{};
 
-class Edge;
-class Vertex;
-class Face;
+  public:
+    class HalfEdge;
+    class Vertex;
+    class Edge;
+    class Face;
 
-class HalfEdge {
-    std::weak_ptr<HalfEdge> twin;
-    std::weak_ptr<HalfEdge> next;
-    std::weak_ptr<Edge> edge;
-    std::weak_ptr<Vertex> vertex;
-    std::weak_ptr<Face> face;
+    using HalfEdgeRef = std::list<HalfEdge>::iterator;
+    using VertexRef = std::list<Vertex>::iterator;
+    using EdgeRef = std::list<Edge>::iterator;
+    using FaceRef = std::list<Face>::iterator;
+
+    class HalfEdge {
+      public:
+        HalfEdgeRef twin;
+        HalfEdgeRef next;
+        EdgeRef edge;
+        VertexRef vertex;
+        FaceRef face;
+
+        size_t id{};
+
+        void set_tnvef(HalfEdgeRef twin_, HalfEdgeRef next_, VertexRef vertex_,
+                       EdgeRef edge_, FaceRef face_) {
+            twin = twin_;
+            next = next_;
+            vertex = vertex_;
+            edge = edge_;
+            face = face_;
+        }
+    };
+
+    class Edge {
+      public:
+        HalfEdgeRef half_edge;
+        size_t id{};
+    };
+
+    class Vertex {
+      public:
+        HalfEdgeRef half_edge;
+        size_t id{};
+    };
+
+    class Face {
+      public:
+        HalfEdgeRef half_edge;
+        size_t id{};
+    };
+
+    std::list<HalfEdge> half_edges;
+    std::list<Vertex> vertices;
+    std::list<Edge> edges;
+    std::list<Face> faces;
+
+    HalfEdgeRef emplace_half_edge();
+    VertexRef emplace_vertex();
+    EdgeRef emplace_edge();
+    FaceRef emplace_face();
+
+    void free_half_edge(HalfEdgeRef h);
+    void free_vertex(VertexRef v);
+    void free_edge(EdgeRef e);
+    void free_face(FaceRef f);
 };
-
-class Edge {
-    std::weak_ptr<HalfEdge> half_edge;
-};
-
-class Vertex {
-    std::weak_ptr<HalfEdge> half_edge;
-};
-
-class Face {
-    std::weak_ptr<HalfEdge> half_edge;
-};
-
-} // namespace hfeg
