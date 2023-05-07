01edf35042ee52c79f50101b5f4cbb88d136c442
Jiajun Han <jiajunhanh@gmail.com>
2023-05-08 01:03:36 +0900

    Add GUI

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ static void glfw_error_callback(int error, const char *description) {
     fmt::print(std::cerr, "GLFW Error {}: {}\n", error, description);
 }
 
-constexpr int n_grid = 3;
+constexpr int n_grid = 8;
 constexpr int n_grid_nodes = (n_grid + 1) * (n_grid + 1);
 constexpr float dx = 1.0f / n_grid;
 
@@ -51,7 +51,8 @@ class Edge {
     size_t j{}; // point id
 };
 
-auto lerp(Vertex &v, const Vertex &vi, const Vertex &vj, float t, int d) {
+static auto lerp(Vertex &v, const Vertex &vi, const Vertex &vj, float t,
+                 int d) {
     auto coord_i = vi.coord(d);
     auto coord_j = vj.coord(d);
     auto coord = coord_i + (coord_j - coord_i) * t;
@@ -60,8 +61,8 @@ auto lerp(Vertex &v, const Vertex &vi, const Vertex &vj, float t, int d) {
     v.b[d] = v.q[d] == 0.0f;
 }
 
-void add_grid_edges(std::vector<Vertex> &cut_vertices,
-                    std::vector<Edge> &cut_edges) {
+static void add_grid_edges(std::vector<Vertex> &cut_vertices,
+                           std::vector<Edge> &cut_edges) {
     std::vector<size_t> grid_cut_vertices[n_grid * n_grid][2];
     for (size_t i = n_grid_nodes; i < cut_vertices.size(); ++i) {
         const auto &v = cut_vertices[i];
@@ -114,7 +115,7 @@ void add_grid_edges(std::vector<Vertex> &cut_vertices,
     }
 }
 
-std::pair<std::vector<Vertex>, std::vector<Edge>>
+static std::pair<std::vector<Vertex>, std::vector<Edge>>
 compute_cut_vertices_and_edges(const std::vector<Vertex> &vertices,
                                const std::vector<Edge> &edges) {
     std::vector<Vertex> cut_vertices;
@@ -187,6 +188,171 @@ compute_cut_vertices_and_edges(const std::vector<Vertex> &vertices,
     return {cut_vertices, cut_edges};
 }
 
+static void show_cut_cell() {
+    constexpr float canvas_width = 600.0f;
+    const ImGuiViewport *main_viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(
+        ImVec2(main_viewport->WorkPos.x + 20, main_viewport->WorkPos.y + 20),
+        ImGuiCond_FirstUseEver);
+
+    if (!ImGui::Begin("Cut-Cell")) {
+        ImGui::End();
+        return;
+    }
+
+    static std::vector<ImVec2> points;
+    static bool opt_enable_grid = true;
+    static bool opt_draw_cut_vertices = true;
+    static bool opt_draw_cut_edges = true;
+    static bool adding_line = false;
+
+    ImGui::Checkbox("Enable grid", &opt_enable_grid);
+    ImGui::Checkbox("Draw cut-vertices", &opt_draw_cut_vertices);
+    ImGui::Checkbox("Draw cut-edges", &opt_draw_cut_edges);
+    ImGui::Text(
+        "Mouse Left: drag to add lines,\nMouse Right: click for context menu.");
+
+    // Using InvisibleButton() as a convenience 1) it will advance the layout
+    // cursor and 2) allows us to use IsItemHovered()/IsItemActive()
+    ImVec2 canvas_p0 =
+        ImGui::GetCursorScreenPos(); // ImDrawList API uses screen coordinates!
+    ImVec2 canvas_sz(canvas_width, canvas_width);
+    ImVec2 canvas_p1 =
+        ImVec2(canvas_p0.x + canvas_sz.x, canvas_p0.y + canvas_sz.y);
+
+    // Draw border and background color
+    ImGuiIO &io = ImGui::GetIO();
+    ImDrawList *draw_list = ImGui::GetWindowDrawList();
+    draw_list->AddRectFilled(canvas_p0, canvas_p1,
+                             IM_COL32(200, 200, 200, 255));
+
+    // This will catch our interactions
+    ImGui::InvisibleButton("canvas", canvas_sz,
+                           ImGuiButtonFlags_MouseButtonLeft |
+                               ImGuiButtonFlags_MouseButtonRight);
+    const bool is_hovered = ImGui::IsItemHovered(); // Hovered
+    const ImVec2 origin(canvas_p0.x, canvas_p0.y);  // Lock scrolled origin
+    const ImVec2 mouse_pos_in_canvas(io.MousePos.x - origin.x,
+                                     io.MousePos.y - origin.y);
+
+    if (is_hovered && !adding_line &&
+        ImGui::IsMouseClicked(ImGuiMouseButton_Left)) {
+        if (points.empty()) {
+            points.emplace_back(mouse_pos_in_canvas);
+        }
+        points.emplace_back(mouse_pos_in_canvas);
+        adding_line = true;
+    }
+    if (adding_line) {
+        points.back() = mouse_pos_in_canvas;
+        if (!ImGui::IsMouseDown(ImGuiMouseButton_Left))
+            adding_line = false;
+    }
+
+    // Context menu (under default mouse threshold)
+    ImGui::OpenPopupOnItemClick("context", ImGuiPopupFlags_MouseButtonRight);
+    if (ImGui::BeginPopup("context")) {
+        if (adding_line)
+            points.resize(points.size() - 1);
+        adding_line = false;
+        if (ImGui::MenuItem("Remove one", nullptr, false, !points.empty())) {
+            points.resize(points.size() - 1);
+            if (points.size() == 1) {
+                points.clear();
+            }
+        }
+        if (ImGui::MenuItem("Remove all", nullptr, false, !points.empty())) {
+            points.clear();
+        }
+        ImGui::EndPopup();
+    }
+
+    // Construct cut-cell
+    std::vector<Vertex> vertices;
+    std::vector<Edge> edges;
+    for (size_t i = 0; i < points.size(); ++i) {
+        Vertex v{};
+        for (int d = 0; d < 2; ++d) {
+            points[i][d] = std::clamp(points[i][d], 0.0f, canvas_width - 0.1f);
+            auto x = points[i][d] / canvas_width * static_cast<float>(n_grid);
+            v.c[d] = static_cast<int>(std::floor(x));
+            v.q[d] = x - std::floor(x);
+            v.b[d] = (v.q[d] == 0.0f);
+        }
+        vertices.emplace_back(v);
+        auto j = (i + 1) % points.size();
+        edges.emplace_back(Edge{i, j});
+    }
+    auto [cut_vertices, cut_edges] =
+        compute_cut_vertices_and_edges(vertices, edges);
+
+    // Draw grid + all lines in the canvas
+    draw_list->PushClipRect(canvas_p0, canvas_p1, true);
+    if (opt_enable_grid) {
+        const float grid_step = canvas_width / static_cast<float>(n_grid);
+        for (int i = 1; i < n_grid; ++i) {
+            draw_list->AddLine(
+                ImVec2(canvas_p0.x + grid_step * static_cast<float>(i),
+                       canvas_p0.y),
+                ImVec2(canvas_p0.x + grid_step * static_cast<float>(i),
+                       canvas_p1.y),
+                IM_COL32(0, 0, 0, 40));
+            draw_list->AddLine(
+                ImVec2(canvas_p0.x,
+                       canvas_p0.y + grid_step * static_cast<float>(i)),
+                ImVec2(canvas_p1.x,
+                       canvas_p0.y + grid_step * static_cast<float>(i)),
+                IM_COL32(0, 0, 0, 40));
+        }
+    }
+    for (size_t n = 0; n + 1 < points.size(); ++n) {
+        draw_list->AddLine(
+            ImVec2(origin.x + points[n].x, origin.y + points[n].y),
+            ImVec2(origin.x + points[n + 1].x, origin.y + points[n + 1].y),
+            IM_COL32(0, 0, 0, 255), 2.0f);
+    }
+    if (points.size() >= 3) {
+        draw_list->AddLine(
+            ImVec2(origin.x + points[0].x, origin.y + points[0].y),
+            ImVec2(origin.x + points[points.size() - 1].x,
+                   origin.y + points[points.size() - 1].y),
+            IM_COL32(0, 0, 0, 255), 2.0f);
+    }
+    if (opt_draw_cut_edges) {
+        for (const auto &e : cut_edges) {
+            if (e.i < n_grid_nodes && e.j < n_grid_nodes) {
+                continue;
+            }
+            const auto &v0 = cut_vertices[e.i];
+            const auto &v1 = cut_vertices[e.j];
+            auto x0 = (static_cast<float>(v0.c[0]) + v0.q[0]) /
+                      static_cast<float>(n_grid) * canvas_width;
+            auto y0 = (static_cast<float>(v0.c[1]) + v0.q[1]) /
+                      static_cast<float>(n_grid) * canvas_width;
+            auto x1 = (static_cast<float>(v1.c[0]) + v1.q[0]) /
+                      static_cast<float>(n_grid) * canvas_width;
+            auto y1 = (static_cast<float>(v1.c[1]) + v1.q[1]) /
+                      static_cast<float>(n_grid) * canvas_width;
+            ImVec2 p0(origin.x + x0, origin.y + y0);
+            ImVec2 p1(origin.x + x1, origin.y + y1);
+            draw_list->AddLine(p0, p1, IM_COL32(0, 0, 0, 255), 2);
+        }
+    }
+    if (opt_draw_cut_vertices) {
+        for (size_t i = n_grid_nodes; i < cut_vertices.size(); ++i) {
+            const auto &v = cut_vertices[i];
+            auto x = (static_cast<float>(v.c[0]) + v.q[0]) /
+                     static_cast<float>(n_grid) * canvas_width;
+            auto y = (static_cast<float>(v.c[1]) + v.q[1]) /
+                     static_cast<float>(n_grid) * canvas_width;
+            draw_list->AddCircleFilled(ImVec2(origin.x + x, origin.y + y), 4,
+                                       IM_COL32(255, 0, 0, 255));
+        }
+    }
+    draw_list->PopClipRect();
+    ImGui::End();
+}
+
 int main(int argc, char *argv[]) {
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
@@ -232,6 +398,7 @@ int main(int argc, char *argv[]) {
         ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
     io.ConfigFlags |=
         ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
+    // io.IniFilename = nullptr;
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
@@ -241,35 +408,6 @@ int main(int argc, char *argv[]) {
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can
-    // also load multiple fonts and use ImGui::PushFont()/PopFont() to select
-    // them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you
-    // need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return a nullptr.
-    // Please handle those errors in your application (e.g. use an assertion, or
-    // display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and
-    // stored into a texture when calling
-    // ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame
-    // below will call.
-    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use
-    // Freetype for higher quality font rendering.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string
-    // literal you need to write a double backslash \\ !
-    // - Our Emscripten build process allows embedding fonts to be accessible at
-    // runtime from the "fonts/" folder. See Makefile.emscripten for details.
-    // io.Fonts->AddFontDefault();
-    // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
-    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    // ImFont* font =
-    // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f,
-    // nullptr, io.Fonts->GetGlyphRangesJapanese()); IM_ASSERT(font != nullptr);
-
     // Our state
     bool show_demo_window = true;
     bool show_another_window = false;
@@ -277,16 +415,6 @@ int main(int argc, char *argv[]) {
 
     // Main loop
     while (!glfwWindowShouldClose(window)) {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to
-        // tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data
-        // to your main application, or clear/overwrite your copy of the mouse
-        // data.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input
-        // data to your main application, or clear/overwrite your copy of the
-        // keyboard data. Generally you may always pass all inputs to dear
-        // imgui, and hide them from your application based on those two flags.
         glfwPollEvents();
 
         // Start the Dear ImGui frame
@@ -294,60 +422,7 @@ int main(int argc, char *argv[]) {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in
-        // ImGui::ShowDemoWindow()! You can browse its code to learn more about
-        // Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End
-        // pair to create a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Begin("Hello, world!"); // Create a window called "Hello,
-                                           // world!" and append into it.
-
-            ImGui::Text(
-                "This is some useful text."); // Display some text (you can use
-                                              // a format strings too)
-            ImGui::Checkbox("Demo Window",
-                            &show_demo_window); // Edit bools storing our window
-                                                // open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat(
-                "float", &f, 0.0f,
-                1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3(
-                "clear color",
-                (float *)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button(
-                    "Button")) // Buttons return true when clicked (most widgets
-                               // return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-                        1000.0f / io.Framerate, io.Framerate);
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if (show_another_window) {
-            ImGui::Begin(
-                "Another Window",
-                &show_another_window); // Pass a pointer to our bool variable
-                                       // (the window will have a closing button
-                                       // that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
+        show_cut_cell();
 
         // Rendering
         ImGui::Render();
@@ -370,46 +445,5 @@ int main(int argc, char *argv[]) {
 
     glfwDestroyWindow(window);
     glfwTerminate();
-
-    if (argc < 2) {
-        return 0;
-    }
-    std::vector<Vertex> vertices;
-    std::vector<Edge> edges;
-    std::ifstream ifs(argv[1]);
-    size_t n, m;
-    ifs >> n >> m;
-    while (n--) {
-        float x[2]{};
-        Vertex v{};
-        for (int d = 0; d < 2; ++d) {
-            ifs >> x[d];
-            assert(x[d] >= 0.0f && x[d] < 1.0f);
-            x[d] *= n_grid;
-            v.c[d] = static_cast<int>(std::floor(x[d]));
-            v.q[d] = x[d] - std::floor(x[d]);
-            v.b[d] = (v.q[d] == 0.0f);
-            // std::cout << v.c[d] << ' ' << v.q[d] << ' ' << v.b[d] << '\n';
-        }
-        vertices.emplace_back(v);
-    }
-    while (m--) {
-        size_t i, j;
-        ifs >> i >> j;
-        edges.emplace_back(i, j);
-    }
-    auto [cut_vertices, cut_edges] =
-        compute_cut_vertices_and_edges(vertices, edges);
-    std::cout << cut_vertices.size() << ' ' << cut_edges.size() << '\n';
-    for (size_t i = 0; i < cut_vertices.size(); ++i) {
-        const auto &v = cut_vertices[i];
-        std::cout << i << ":\n";
-        for (int d = 0; d < 2; ++d) {
-            std::cout << v.c[d] << ' ' << v.q[d] << ' ' << v.b[d] << '\n';
-        }
-    }
-    for (const auto &e : cut_edges) {
-        std::cout << e.i << ' ' << e.j << '\n';
-    }
     return 0;
 }
